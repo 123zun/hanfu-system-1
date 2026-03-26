415cbce4705b020780a9d89b6cb1a561092d9fda
张伟 <3361466487@qq.com>
2026-03-26 21:08:27 +0800

    1

---
+++ client/hanfucommunity/vite.config.js
@@ -0,0 +1,28 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  server: {
+    port: 3000, // 前端开发服务器端口
+    proxy: {
+      '/api': { // 所有以/api开头的请求
+        target: 'http://localhost:8080', // 后端服务地址
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '') // 可选，重写请求路径
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
